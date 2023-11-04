1ec0d8f1c72c10cd907a5de79f0bab3c1fab64be
Haryanto <gendang_telinga@yahoo.com>
2023-11-05 06:38:13 +0800

    add documentation to sh_build_demo

--- sh_build_demo.sh
+++ sh_build_demo.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
+
+# file ini berguna untuk membuild express_demo
+# sehingga hasilnya akan berada di folder demo_release
+# yang bisa dijalankan sebagaimana aplikasi pada umumnya
+
+# This file is used to build the express_demo
+# so that the result will be in the demo_release folder
+# which can be run like a typical application
+
+
 pkg ./demo/express_demo/package.json
-
-
 ./s_copy_release.sh
 
 binfolder="./bin"
